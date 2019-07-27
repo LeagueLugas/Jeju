98f526b42cc18cf56e24d8d1e730f8ec058dd702
LeagueLugas <blackblack9685@gmail.com>
2019-07-28 00:12:14 +0900

    Create Index
    create indeex.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+﻿<html>
 <head>
 	<title>Jeju</title>
 	<link rel="stylesheet" type="text/css" href="skin/main.css">
@@ -12,10 +12,35 @@
 		<div id="main_head">
 			<img src="imgs/banner_1.png" />
 		</div>
+		<div id="main_content">
+			<article>
+				<div id="main_content_header">
+					<span><strong>구인 게시판</strong></span>
+					<img src="imgs/plus_icon.png" />
+				</div>
+				<div id="main_content_content">
+					<ul>
+						<li>
+							<div class="main_content_title">이거슨 용성이의 처뻔째 게시글 이다</div>
+							<div class="main_content_date">19.07.26</div>
+						</li>
+						<li>
+							<div class="main_content_title">마늘 캐기 알바 하실?</div>
+							<div class="main_content_date">19.07.27</div>
+						</li>
+						<li>
+							<div class="main_content_title">귤 나르실 뿐? 시급 10000원!</div>
+							<div class="main_content_date">19.07.28</div>
+						</li>
+					</ul>
+				</div>
+			</article>
+		</div>
 	</main>
 	
 	<footer>
-		
+		<div><img src="imgs/logo.png" /></div>
+		<span>COPYRIGHT LeagueLugas, ALL RIGHT RESERVED.</span>
 	</footer>
 </body>
 </html>
