d1c4877dba6d3b2026e6ac0ff178657ce66ba6fa
LeagueLugas <blackblack9685@gmail.com>
2019-07-27 22:11:38 +0900

    Start
    start!!!

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Jeju</title>
+	<link rel="stylesheet" type="text/css" href="skin/main.css">
+</head>
+<body>
+	<header>
+		<img src="imgs/logo.png" />
+	</header>
+	
+	<main>
+		<div id="main_head">
+			<img src="imgs/banner_1.png" />
+		</div>
+	</main>
+	
+	<footer>
+		
+	</footer>
+</body>
+</html>
